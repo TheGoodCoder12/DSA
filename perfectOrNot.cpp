c30d4567a77b998d42d6106315e80415e2c28644
TheGoodCoder12 <satyam.strange93@gmail.com>
2023-12-16 16:06:20 +0530

    Error code: need to correct this

---
+++ perfectOrNot.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int &N;
+bool perfect(int &array[&N][&N]){
+    int diagonal;
+    if (array[0][0]==0)
+    {
+        return false;
+    }
+      else
+        {
+            diagonal=array[0][0];
+        }
+    
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (i==j && array[i][j]!=diagonal)
+            {
+                break;
+                return false;
+            }
+            else
+            {
+                if (array[i][j]!=0)
+                {
+                    break;
+                    return false;
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
+
+int main(){
+    N=3;
+    int array[3][3]={{1,0,0},{0,1,0},{0,0,1}}; 
+    cout<<perfect(array[N][N]);
+    return 0;
+}
