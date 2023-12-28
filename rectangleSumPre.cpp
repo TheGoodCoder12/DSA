64ecb29412836a8fe788f57fed02759d4a0f641c
TheGoodCoder12 <satyam.strange93@gmail.com>
2023-12-29 01:06:03 +0530

    Started the work on pre-processing approach, it'll publish soon

---
+++ rectangleSumPre.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int rectangleSumPre(){
+    int ans=0;
+    return ans;
+}
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+
+    //entering the array
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            /* code */
+        }
+        
+    }
+    
+    return 0;
+}
