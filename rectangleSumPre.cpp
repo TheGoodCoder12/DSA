fd00cd98c8f4a6f8da440f5ad50a5ecd77a59150
TheGoodCoder12 <satyam.strange93@gmail.com>
2023-12-29 20:38:29 +0530

    2nd approach using prefix Sum added

--- rectangleSumPre.cpp
+++ rectangleSumPre.cpp
@@ -3,24 +3,52 @@
 
 using namespace std;
 
-int rectangleSumPre(){
-    int ans=0;
-    return ans;
+int rectangleSumPre(vector<vector<int>> & matrix, int l1, int r1, int l2, int r2){
+    int sum=0;
+    //prefix sum array row-wise
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 1; j < matrix[0].size(); j++)
+        {
+            matrix[i][j]+=matrix[i][j-1];
+        }
+        
+    }
+    for (int i = l1; i <=l2; i++)
+    {
+        if (r1!=0)
+        {
+        sum+=(matrix[i][r2]-matrix[i][r1-1]);
+        }
+        else
+        {
+            //matrix[i][r1-1] = 0
+            sum+=matrix[i][r2];
+        }
+        
+    }
+    
+    return sum;
 }
 
 int main(){
     int n,m;
     cin>>n>>m;
+    vector<vector<int>> matrix(n, vector<int>(m));
 
-    //entering the array
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            /* code */
+            cin>>matrix[i][j];
         }
         
     }
-    
+    int l1,r1,l2,r2;
+    cin>>l1>>r1>>l2>>r2;
+
+    int sum=rectangleSumPre(matrix, l1, r1, l2 ,r2);
+    cout<<sum;
     return 0;
+
 }
